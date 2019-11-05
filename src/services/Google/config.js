b23ecac238a28de08ee53d567669229b7c30c984
modgera <modgera@gmail.com>
2019-11-05 11:53:24 +0500

    google api key

--- src/services/Google/config.js
+++ src/services/Google/config.js
@@ -1,7 +1,7 @@
 export const baseUrl = 'https://maps.googleapis.com/maps/api/js?';
 
 export const mapsParams = {
-  key: 'AIzaSyDBMsCfgUYRwzEj-CEX17r-CYKyYiJk-L4',
+  key: 'AIzaSyByqU3ruwiUrAOP46BuOFJCG_5OUiM8ynA',
   language: 'ru',
   region: 'RU',
 };
